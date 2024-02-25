8b23bf83e3b8b4fcf765eb8e503fe83671b13800
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-25 11:38:27 -0500

    got input varibales to tarnsfer to new pdf and open in window. baseline functionlity acehviedd const { app, BrowserWindow, ipcMain } = require('electron'); const path = require('path'); const fs = require('fs').promises; const { PDFDocument } = require('pdf-lib');
    const createWindow = () => {
      const mainWindow = new BrowserWindow({
        width: 800,
        height: 600,
        webPreferences: {
          nodeIntegration: true,
          contextIsolation: false,
          preload: path.join(__dirname, 'preload.js'),
        },
      });
    
      mainWindow.loadFile(path.join(__dirname, 'index.html'));
    };
    
    app.on('ready', createWindow);
    
    app.on('window-all-closed', () => {
      if (process.platform !== 'darwin') {
        app.quit();
      }
    });
    
    app.on('activate', () => {
      if (BrowserWindow.getAllWindows().length === 0) {
        createWindow();
      }
    });
    
    ipcMain.on('generate-pdf', async (event, { templatePath, clientName }) => {
        const pdfBytes = await fs.readFile(path.join(__dirname, '../templates', templatePath));
    
        const pdfDoc = await PDFDocument.load(pdfBytes);
        const form = pdfDoc.getForm();
        const textField = form.getTextField('client');
        textField.setText(clientName);
    
        const modifiedPdfBytes = await pdfDoc.save();
    
        event.sender.send('pdf-generated', modifiedPdfBytes);
    });

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
+const fs = require('fs').promises;
+const { PDFDocument } = require('pdf-lib');
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -36,12 +26,20 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+ipcMain.on('generate-pdf', async (event, { templatePath, clientName }) => {
+    const pdfBytes = await fs.readFile(path.join(__dirname, '../templates', templatePath));
+
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const form = pdfDoc.getForm();
+    const textField = form.getTextField('client');
+    textField.setText(clientName);
+
+    const modifiedPdfBytes = await pdfDoc.save();
+
+    event.sender.send('pdf-generated', modifiedPdfBytes);
+});

c4864de9a320500fbc92c12b9056272e70e7a513
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-26 09:45:42 -0500

    added the rest of the form field functiolities, completely filling out the generated pdf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require('fs').promises;
-const { PDFDocument } = require('pdf-lib');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs").promises;
+const { PDFDocument } = require("pdf-lib");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -10,36 +10,104 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 };
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-ipcMain.on('generate-pdf', async (event, { templatePath, clientName }) => {
-    const pdfBytes = await fs.readFile(path.join(__dirname, '../templates', templatePath));
+ipcMain.on(
+  "generate-pdf",
+  async (
+    event,
+    {
+      templatePath,
+      clientName,
+      assetDescription,
+      VIN_serialNum,
+      licensePlate,
+      registeredOwner,
+      lienHolder,
+      daysOfStorage,
+      storageRate,
+      amountOfArrears,
+      daysOfStorageCost,
+      bailiffCosts,
+      totalOfStorageRate,
+      towingCost,
+      storageCosts,
+      NOICosts,
+      HSTOnCosts,
+      totalCostsToDate,
+      amountDueToRedeem,
+      closingDate,
+      formDate,
+      dateOfAdditionalCharges,
+      assestDescription,
+      repoDate,
+      dateNOISent,
+      staffAffidavitName,
+      registeredOwnerCont,
+      descriptionOfAssest,
+      Affidavit_Date1,
+      Affidavit_Date2,
+    }
+  ) => {
+    const pdfBytes = await fs.readFile(
+      path.join(__dirname, "../templates", templatePath)
+    );
 
     const pdfDoc = await PDFDocument.load(pdfBytes);
     const form = pdfDoc.getForm();
-    const textField = form.getTextField('client');
-    textField.setText(clientName);
+
+    // Set values for all text fields
+    form.getTextField("client").setText(clientName);
+    form.getTextField("assetDescription").setText(assetDescription);
+    form.getTextField("VIN/serialNum").setText(VIN_serialNum);
+    form.getTextField("licensePlate").setText(licensePlate);
+    form.getTextField("registeredOwner").setText(registeredOwner);
+    form.getTextField("lienHolder").setText(lienHolder);
+    form.getTextField("daysOfStorage").setText(daysOfStorage);
+    form.getTextField("storageRate").setText(storageRate);
+    form.getTextField("amountOfArrears").setText(amountOfArrears);
+    form.getTextField("daysOfStorageCost").setText(daysOfStorageCost);
+    form.getTextField("bailiffCosts").setText(bailiffCosts);
+    form.getTextField("totalOfStorageRate").setText(totalOfStorageRate);
+    form.getTextField("towingCost").setText(towingCost);
+    form.getTextField("storageCosts").setText(storageCosts);
+    form.getTextField("NOICosts").setText(NOICosts);
+    form.getTextField("HSTOnCosts").setText(HSTOnCosts);
+    form.getTextField("totalCostsToDate").setText(totalCostsToDate);
+    form.getTextField("amountDueToRedeem").setText(amountDueToRedeem);
+    form.getTextField("closingDate").setText(closingDate);
+    form.getTextField("formDate").setText(formDate);
+    form
+      .getTextField("dateOfAdditionalCharges")
+      .setText(dateOfAdditionalCharges);
+    form.getTextField("repoDate").setText(repoDate);
+    form.getTextField("dateNOISent").setText(dateNOISent);
+    form.getTextField("staffAffidavitName").setText(staffAffidavitName);
+    form.getTextField("registeredOwnerCont").setText(registeredOwnerCont);
+    form.getTextField("Affidavit_Date1").setText(Affidavit_Date1);
+    form.getTextField("Affidavit_Date2").setText(Affidavit_Date2);
 
     const modifiedPdfBytes = await pdfDoc.save();
 
-    event.sender.send('pdf-generated', modifiedPdfBytes);
-});
+    event.sender.send("pdf-generated", modifiedPdfBytes);
+  }
+);
